e66968e6fa241f51530f5c9c4b9a08830eeda67c
Mykola Vasyl'yev <mykovasyl@gmail.com>
2021-10-31 20:59:23 -0400

    commit tree ac019cce77dc297e65b97e8d46888244095a4886

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance) {
+  if (rideDistance <= 400) {
+    return `This one is on me!`;
+  } else if (2000 < rideDistance && rideDistance < 2500) {
+    return `I will gladly take your thirty bucks.`;
+  } else if (rideDistance >= 2500) {
+    return `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destCity) {
+  let destMessage = destCity === `NYC` ? `Ok, sounds good.` : `No go.`;
+  return destMessage
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount) {
+  switch (tipAmount) {
+    case `generous`:
+      return `Thank you so much.`;
+      break;
+    case `not as generous`:
+      return `Thank you.`;
+      break;
+    default:
+      return `Bye.`;
+      break;
+  }
 }
